ae1cddc79db786d3ab2c9d5b79686ca8261a5f61
Pavel_Kasapov <pavel.kasapov.95@gmail.com>
2026-03-10 17:26:40 +0300

    - Setup input adapter;

--- Assets/Features/Camera/Camera3D/Scripts/InputAdapter.cs
+++ Assets/Features/Camera/Camera3D/Scripts/InputAdapter.cs
@@ -1,10 +1,33 @@
-using Cysharp.Threading.Tasks;
+using R3;
+using ReactiveInputSystem;
+using System;
 using UnityEngine;
-
 
 namespace MechanicsPlayground.Camera3D
 {
-    public class InputAdapter
-    { 
+    public class InputAdapter : IDisposable
+    {
+        private readonly Camera3DInputActions _inputActions;
+        public Observable<Vector2> Move => _inputActions.Camera3D.Move.PerformedAsObservable()
+                .Merge(_inputActions.Camera3D.Move.CanceledAsObservable())
+                .Select(ctx => ctx.ReadValue<Vector2>());
+        public Observable<Vector2> Look => _inputActions.Camera3D.Look.PerformedAsObservable()
+                .Merge(_inputActions.Camera3D.Move.CanceledAsObservable())
+                .Select(ctx => ctx.ReadValue<Vector2>());
+        public Observable<float> Zoom => _inputActions.Camera3D.Zoom.PerformedAsObservable()
+                .Merge(_inputActions.Camera3D.Move.CanceledAsObservable())
+                .Select(ctx => ctx.ReadValue<float>());
+
+        public InputAdapter()
+        {
+            _inputActions = new();
+            _inputActions.Camera3D.Enable();
+        }
+
+        public void Dispose()
+        {
+            _inputActions.Camera3D.Disable();
+            _inputActions.Dispose();
+        }
     }
 }
