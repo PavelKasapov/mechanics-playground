42e68434f6e3da8bb8278e00425255fd11d55328
Pavel_Kasapov <pavel.kasapov.95@gmail.com>
2026-08-25 10:05:46 +0300

    - Rework ground check;

--- Assets/Core/Scripst/PlayerModel/GroundedProvider.cs
+++ Assets/Core/Scripst/PlayerModel/GroundedProvider.cs
@@ -2,21 +2,35 @@ using UnityEngine;
 
 public class GroundedProvider : MonoBehaviour
 {
-    [SerializeField] private Collider _collider;
+    [SerializeField] private float _slopeAngle = 45f;
+    private readonly Vector3 _planeNormal = Vector3.up;
+    private float _slopeCos = 0.707f;
     public bool IsGrounded { get; private set; }
 
     private void FixedUpdate()
     {
-        IsGrounded = true;
-    }
-
-    /*private void OnCollisionEnter(Collision collision)
-    {
-        IsGrounded = true;
-    }
-
-    private void OnCollisionExit(Collision collision)
-    {
         IsGrounded = false;
-    }*/
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (IsGrounded)
+            return;
+
+        foreach (var contact in collision.contacts)
+        {
+            float dot = Vector3.Dot(contact.normal, _planeNormal);
+            bool isVertical = dot > _slopeCos;
+            if (isVertical)
+            {
+                IsGrounded = true;
+                return;
+            }    
+        }
+    }
+
+    private void OnValidate()
+    {
+        _slopeCos = Mathf.Cos(_slopeAngle * Mathf.Deg2Rad);
+    }
 }
